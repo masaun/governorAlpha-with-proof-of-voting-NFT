04f9576e7f236e06d2c6ed88b7698b97510f4e55
masaun <masanoriuno@gmail.com>
2021-09-02 22:03:35 +0900

    [SC]: Define the ProofOfVotingNFTFactory.sol

---
+++ contracts/ProofOfVotingNFTFactory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @notice - This is the factory smart contract of NFT that represents a proof of voting
+ */
+contract ProofOfVotingNFTFactory {
+
+    constructor(string memory name, string memory symbol) public {}
+
+}
