15749d7396fc8069ff4a6d83646a42a1999e784e
masaun <masanoriuno@gmail.com>
2021-09-02 22:01:35 +0900

    [SC]: the Define ProofOfVotingNFT.sol

---
+++ contracts/ProofOfVotingNFT.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @notice - This is the smart contract of NFT that represents a proof of voting
+ */
+contract ProofOfVotingNFT {
+
+    constructor(string memory name, string memory symbol) public {}
+
+    function getCountOfProofOfVotingNFTs(address voter) public view returns (uint _proofOfVotingNFTs) {
+        // [Todo]: Return number of NFTs which voter has
+    }
+}
