bce0d6dc7eaaa3ec43bf742c6b27365861aaed4a
masaun <masanoriuno@gmail.com>
2021-09-03 09:25:57 +0900

    [SC]: Inherit ERC721Full into the ProofOfVotingNFT.sol

--- contracts/ProofOfVotingNFT.sol
+++ contracts/ProofOfVotingNFT.sol
@@ -1,14 +1,29 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
+import { ERC721Full } from "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import { Counters } from "@openzeppelin/contracts/drafts/Counters.sol";
+
 /**
  * @notice - This is the smart contract of NFT that represents a proof of voting
  */
-contract ProofOfVotingNFT {
+contract ProofOfVotingNFT is ERC721Full {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    constructor(string memory name, string memory symbol) public {}
+    constructor() ERC721Full("Proof of voting NFT", "POV") public {}
 
     function getCountOfProofOfVotingNFTs(address voter) public view returns (uint _proofOfVotingNFTs) {
         // [Todo]: Return number of NFTs which voter has
     }
+
+    function mintProofOfVotingNFTs(address voter) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newNftId = _tokenIds.current();
+        _mint(voter, newNftId);
+
+        return newNftId;
+    }
+
 }
