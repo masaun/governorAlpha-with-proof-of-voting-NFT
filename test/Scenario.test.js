7ab812bb45642fc10a6c6313fdbee37a5ed4d46e
masaun <masanoriuno@gmail.com>
2021-09-03 21:59:07 +0900

    [Test]: Separate each parts by adding describe()

--- test/Scenario.test.js
+++ test/Scenario.test.js
@@ -23,69 +23,77 @@ describe("Scenario test", function() {
     let Comp, comp, COMP
     let GovernorAlpha, governorAlpha, GOVERNOR_ALPHA
 
-    it("Assign accounts", async function() {
-        accounts = await hre.ethers.getSigners()
-        guardian = accounts[0].address
-        deployer = accounts[0].address
-        user1 = accounts[1].address
+    describe("Deploy smart contracts", function() {
+        it("Assign accounts", async function() {
+            accounts = await hre.ethers.getSigners()
+            guardian = accounts[0].address
+            deployer = accounts[0].address
+            user1 = accounts[1].address
+        })
+
+        it("Deploy the Timelock.sol", async function() {
+            const admin = deployer
+            const delay = String(oneWeekInSeconds)
+            console.log('=== delay ===', delay)
+
+            Timelock = await ethers.getContractFactory("Timelock")
+            timelock = await Timelock.deploy(admin, delay)
+            TIMELOCK = timelock.address 
+        })
+
+        it("Deploy the Comp.sol", async function() {
+            const account = deployer
+
+            Comp = await ethers.getContractFactory("Comp")
+            comp = await Comp.deploy(account)
+            COMP = comp.address
+        })
+
+        it("Deploy the ProofOfVotingNFTFactory.sol", async function() {
+            ProofOfVotingNFTFactory = await ethers.getContractFactory("ProofOfVotingNFTFactory")
+            proofOfVotingNFTFactory = await ProofOfVotingNFTFactory.deploy()
+            PROOF_OF_VOTING_NFT_FACTORY = proofOfVotingNFTFactory.address
+        })
+
+        it("Deploy the GovernorAlpha.sol", async function() {
+            GovernorAlpha = await ethers.getContractFactory("contracts/compound/Governance/GovernorAlpha.sol:GovernorAlpha")
+            governorAlpha = await GovernorAlpha.deploy(TIMELOCK, COMP, guardian, PROOF_OF_VOTING_NFT_FACTORY)
+            GOVERNOR_ALPHA = governorAlpha.address
+        })
     })
 
-    it("Deploy the Timelock.sol", async function() {
-        const admin = deployer
-        const delay = String(oneWeekInSeconds)
-        console.log('=== delay ===', delay)
+    describe("Check status before scenario test is started", function() {
+        it("Should return the Comp balance", async function() {
+            let _compBalance = await comp.balanceOf(deployer)
+            let compBalance = ethers.utils.formatEther(String(_compBalance))
+            console.log('=== Comp balance of deployer ===',  compBalance) // fromETH
+            expect(compBalance).to.equal("10000000.0")  /// 10,000,000 COMP
+        })
 
-        Timelock = await ethers.getContractFactory("Timelock")
-        timelock = await Timelock.deploy(admin, delay)
-        TIMELOCK = timelock.address 
-    })
-
-    it("Deploy the Comp.sol", async function() {
-        const account = deployer
-
-        Comp = await ethers.getContractFactory("Comp")
-        comp = await Comp.deploy(account)
-        COMP = comp.address
-    })
-
-    it("Deploy the ProofOfVotingNFTFactory.sol", async function() {
-        ProofOfVotingNFTFactory = await ethers.getContractFactory("ProofOfVotingNFTFactory")
-        proofOfVotingNFTFactory = await ProofOfVotingNFTFactory.deploy()
-        PROOF_OF_VOTING_NFT_FACTORY = proofOfVotingNFTFactory.address
-    })
-
-    it("Deploy the GovernorAlpha.sol", async function() {
-        GovernorAlpha = await ethers.getContractFactory("contracts/compound/Governance/GovernorAlpha.sol:GovernorAlpha")
-        governorAlpha = await GovernorAlpha.deploy(TIMELOCK, COMP, guardian, PROOF_OF_VOTING_NFT_FACTORY)
-        GOVERNOR_ALPHA = governorAlpha.address
-    })
-
-    it("Should return the Comp balance", async function() {
-        let _compBalance = await comp.balanceOf(deployer)
-        let compBalance = ethers.utils.formatEther(String(_compBalance))
-        console.log('=== Comp balance of deployer ===',  compBalance) // fromETH
-        expect(compBalance).to.equal("10000000.0")  /// 10,000,000 COMP
-    })
-
-    it("Should return the Comp balance", async function() {
-        const dst = user1
-        const rawAmount = ethers.utils.parseEther('2000')  /// 2000 COMP
-        let txReceipt2 = await comp.transfer(dst, rawAmount)
-        console.log('=== txReceipt of comp.transfer() ===', txReceipt2)
+        it("Should return the Comp balance", async function() {
+            const dst = user1
+            const rawAmount = ethers.utils.parseEther('2000')  /// 2000 COMP
+            let txReceipt2 = await comp.transfer(dst, rawAmount)
+            console.log('=== txReceipt of comp.transfer() ===', txReceipt2)
+        })
     })
 
 
     ///-----------------------------------------
     /// General process (propose - cast voting)
     ///-----------------------------------------
-    it("Propose a proposal", async function() {})
+    describe("General process (propose - cast voting)", function() {
+        it("Propose a proposal", async function() {})
 
-    it("Cast voting and distribute NFTs into voters (wallets)", async function() {})
+        it("Cast voting and distribute NFTs into voters (wallets)", async function() {})
+    })
 
 
     ///---------------------------------
     ///  Rewards distribution process
     ///---------------------------------
-    it("Distribute rewards (COMP Tokens) into voters (wallets) depends on number of NFTs that each voters has ", async function() {})
+    describe("Rewards distribution process", function() {
+        it("Distribute rewards (COMP Tokens) into voters (wallets) depends on number of NFTs that each voters has ", async function() {})
+    })
 
 })
