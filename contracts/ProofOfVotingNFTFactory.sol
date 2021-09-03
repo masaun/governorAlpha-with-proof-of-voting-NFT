eff6645965dd4d9098aa89ab5d6a6a3261f9e4ac
masaun <masanoriuno@gmail.com>
2021-09-03 09:54:16 +0900

    [SC]: Define createNewProofOfVotingNFT() in the ProofOfVotingNFTFactory.sol

--- contracts/ProofOfVotingNFTFactory.sol
+++ contracts/ProofOfVotingNFTFactory.sol
@@ -1,11 +1,21 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
+import { ProofOfVotingNFT } from "./ProofOfVotingNFT.sol";
+
+
 /**
  * @notice - This is the factory smart contract of NFT that represents a proof of voting
  */
 contract ProofOfVotingNFTFactory {
 
-    constructor(string memory name, string memory symbol) public {}
+    address[] public proofOfVotingNFTAddresses;
+
+    constructor() public {}
+
+    function createNewProofOfVotingNFT() public returns (bool) {
+        ProofOfVotingNFT proofOfVotingNFT = new ProofOfVotingNFT();
+        proofOfVotingNFTAddresses.push(address(proofOfVotingNFT));
+    }
 
 }
