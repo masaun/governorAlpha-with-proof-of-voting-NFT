921b94086353982ec2d534cbd5cddeb31e91c0ea
masaun <masanoriuno@gmail.com>
2021-09-04 21:40:25 +0900

    [Test]: Replace RPC-endpoint from localhost to mainnet(-fork)

--- test/web3js-helper/web3jsHelper.js
+++ test/web3js-helper/web3jsHelper.js
@@ -1,8 +1,11 @@
 "use strict";
 
+require('dotenv').config()
+
 /// Using local network
 const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'))
+//const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'))
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`));
 const ethers = require('ethers')
 
 /// Openzeppelin test-helper
