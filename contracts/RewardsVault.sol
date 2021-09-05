b5e58ddba01996cdc308ed35949f4801f0a68ff3
masaun <masanoriuno@gmail.com>
2021-09-05 21:53:16 +0900

    [SC]: Define depositRewardToken() and distributeRewardToken() in the RewardsVault.sol

--- contracts/RewardsVault.sol
+++ contracts/RewardsVault.sol
@@ -15,4 +15,20 @@ contract RewardsVault {
         comp = _comp;
     }
 
+    /**
+     * @notice - Deposit some COMP tokens into the this contract (Rewards Vault)
+     */
+    function depositRewardToken(uint depositedAmount) public {
+        // [Note]: In advance, msg.sender need to approve to transfer COMP
+        comp.transferFrom(msg.sender, address(this), depositedAmount);
+    }
+
+    /**
+     * @notice - Distribute rewards (COMP Tokens) into voters (wallets) depends on number of NFTs that each voters has.
+     */ 
+    function distributeRewardToken(address voter, uint distributedAmount) public {
+        // [Todo]: Add a method for counting NFTs
+
+        comp.transfer(voter, distributedAmount);
+    }
 }
